75867a454d69a1aef036dbf9758859f5bcc1a7c2
Ritesh Ubale <riteshubale563@gmail.com>
2026-03-09 23:27:57 +0530

    Complete authentication system with register and login endpoints

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from .database import Base
-
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
@@ -9,4 +9,13 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-    
+
+class Contest(Base):
+    __tablename__ = "contests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    created_by = Column(Integer)
